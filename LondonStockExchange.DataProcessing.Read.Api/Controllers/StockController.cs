bae11edef5568affc81338a03ea05386313aea91
fcl-93 <fabio_cl93_@hotmail.com>
2022-02-15 19:29:55 +0000

    Define Reader API (According with the architecture defined)

---
+++ LondonStockExchange.DataProcessing.Read.Api/Controllers/StockController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LondonStockExchange.DataProcessing.Read.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StockController : ControllerBase
+    {
+       private readonly ILogger<StockController> _logger;
+
+        public StockController(ILogger<StockController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("value")]
+        public IEnumerable<WeatherForecast> GetStockValueByTickerSymbol(string tickerSymbol)
+        {
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = Random.Shared.Next(-20, 55),
+            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("value/all")]
+        public IEnumerable<dynamic> GetAllStockValues()
+        {
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = Random.Shared.Next(-20, 55),
+            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("value")]
+        public IEnumerable<dynamic> GetStockValueByTickerSymbols(IList<string> tickerSymbols)
+        {
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = Random.Shared.Next(-20, 55),
+            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+            throw new NotImplementedException();
+        }
+    }
+}
