1812925bba2b7a0f5691b7d7153538a683f45565
fcl-93 <fabio_cl93_@hotmail.com>
2022-02-15 20:24:23 +0000

    Define model

---
+++ LondonStockExchange.DataProcessing.Contracts/TransactionPlaced.cs
@@ -0,0 +1,12 @@
+﻿namespace LondonStockExchange.DataProcessing.Contracts
+{
+    public class TransactionPlaced
+    {
+        public string TickerSymbol { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; }
+        public decimal ShareNumber { get; set; }
+        public string BrokerId { get; set; }
+        public DateTime TradeDateTime { get; set; }
+    }
+}
