e144f00f5d6ddada35fd0a879a36f66274b7a5f7
fcl-93 <fabio_cl93_@hotmail.com>
2022-02-15 22:55:21 +0000

    Introduce Paging on query that may return huge amount of data.

--- LondonStockExchange.DataProcessing.Read.Api/Controllers/StockController.cs
+++ LondonStockExchange.DataProcessing.Read.Api/Controllers/StockController.cs
@@ -16,8 +16,13 @@ namespace LondonStockExchange.DataProcessing.Read.Api.Controllers
         }
 
         [HttpGet("value")]
-        public async Task<dynamic> GetStockValueByTickerSymbolAsync(string tickerSymbol)
+        public async Task<ActionResult<dynamic>> GetStockValueByTickerSymbolAsync(string tickerSymbol)
         {
+            if(string.IsNullOrEmpty(tickerSymbol))
+            {
+                return BadRequest("TickerSymbol cannot be null or empty.");
+            }
+
             var sql = @"SELECT TOP 1 [TickerSymbol], [TradeDateTime], [Price],[Currency] 
                         FROM[LondonStockExchange_Transactions_Writes].[dbo].[Transactions]
                         WHERE TickerSymbol = @TickerSymbol
@@ -26,26 +31,43 @@ namespace LondonStockExchange.DataProcessing.Read.Api.Controllers
             await using var connection = new SqlConnection("Server=localhost;Database=LondonStockExchange_Transactions_Writes;Integrated Security=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=True");
             await connection.OpenAsync();
 
-            var lastStockPrice = await connection.QueryAsync<object>(sql, new { TickerSymbol = tickerSymbol});
+            var lastStockPrice = await connection.QueryFirstOrDefaultAsync<object>(sql, new { TickerSymbol = tickerSymbol});
 
             return Ok(lastStockPrice);
         }
 
-        //[HttpGet("value/all")]
-        //public IEnumerable<dynamic> GetAllStockValues()
-        //{
-        //    //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    //{
-        //    //    Date = DateTime.Now.AddDays(index),
-        //    //    TemperatureC = Random.Shared.Next(-20, 55),
-        //    //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //    //})
-        //    //.ToArray();
-        //    throw new NotImplementedException();
-        //}
 
-        [HttpGet("/stocks/values")]
-        public async Task<IEnumerable<dynamic>> GetStockValueByTickerSymbols([FromQuery] IList<string> tickerSymbols)
+        [HttpGet("/stocks/value/all")]
+        public async Task<ActionResult<IEnumerable<dynamic>>> GetAllStockValues(int pageNumber, int pageSize)
+        {
+            if (pageNumber <= 0) { return BadRequest("PageNumber less than 1 is not allowed."); }
+            if (pageSize <= 0){ return BadRequest("PageSize less than 1 is not allowed"); }
+            if (pageSize > 50){ return BadRequest("PageSize more than 50 is not alloweded."); }
+            
+            var sql = @"Select t1.TickerSymbol, t1.Price, t1.Currency, t1.TradeDateTime
+                        from [LondonStockExchange_Transactions_Writes].[dbo].[Transactions] as t1
+                        inner join 
+                        (
+                            SELECT t2.TickerSymbol, MAX(t2.[TradeDateTime]) as LastestDate
+		                    FROM [LondonStockExchange_Transactions_Writes].[dbo].[Transactions] as t2
+		                    Group by t2.TickerSymbol 
+					        Order By t2.TickerSymbol
+					        OFFSET (@PageNumber-1)*@PageSize ROWS
+					        FETCH NEXT @PageSize ROWS ONLY
+                        ) t 
+                        on t1.TradeDateTime = t.LastestDate
+                        and t1.TickerSymbol = t.TickerSymbol";
+
+            await using var connection = new SqlConnection("Server=localhost;Database=LondonStockExchange_Transactions_Writes;Integrated Security=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=True");
+            await connection.OpenAsync();
+
+            var lastStockPricesForTickers = await connection.QueryAsync<object>(sql, new { PageNumber = pageNumber, PageSize = pageSize });
+
+            return Ok(lastStockPricesForTickers);
+        }
+
+        [HttpGet("/stocks/value")]
+        public async Task<ActionResult<IEnumerable<dynamic>>> GetStockValueByTickerSymbols([FromQuery] IList<string> tickerSymbols)
         {
             var sql = @"
                 Select t1.TickerSymbol, t1.Price, t1.Currency, t1.TradeDateTime
@@ -65,7 +87,8 @@ namespace LondonStockExchange.DataProcessing.Read.Api.Controllers
             await connection.OpenAsync();
 
             var lastStockPricesForTickers = await connection.QueryAsync<object>(sql, new { TickerSymbols = tickerSymbols.ToArray()});
-            throw new NotImplementedException();
+           
+            return Ok(lastStockPricesForTickers);
         }
     }
 }
