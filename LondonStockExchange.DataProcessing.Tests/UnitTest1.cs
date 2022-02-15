886c6c1c1a95b691a8d5411e07344fcb2a2e8de5
fcl-93 <fabio_cl93_@hotmail.com>
2022-02-15 20:25:03 +0000

    Add Testing Solution

---
+++ LondonStockExchange.DataProcessing.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LondonStockExchange.DataProcessing.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
