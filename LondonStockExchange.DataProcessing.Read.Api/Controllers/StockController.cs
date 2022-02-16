04368dce2ed41c9704dda944d0d6f2b3acfc497b
fcl-93 <fabio_cl93_@hotmail.com>
2022-02-16 18:44:11 +0000

    Refactor StockController introduce Repository

--- LondonStockExchange.DataProcessing.Read.Api/Controllers/StockController.cs
+++ LondonStockExchange.DataProcessing.Read.Api/Controllers/StockController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Dapper;
+using LondonStockExchange.DataProcessing.Read.Api.Infrastructure.Repository;
 
 namespace LondonStockExchange.DataProcessing.Read.Api.Controllers
 {
@@ -8,11 +9,17 @@ namespace LondonStockExchange.DataProcessing.Read.Api.Controllers
     [Route("[controller]")]
     public class StockController : ControllerBase
     {
-       private readonly ILogger<StockController> _logger;
+        private readonly IStockRepository stockRepository;
+        private readonly ILogger<StockController> _logger;
 
-        public StockController(ILogger<StockController> logger)
+        public StockController(IStockRepository stockRepository, ILogger<StockController> logger)
         {
+            this.stockRepository=stockRepository;
             _logger = logger;
+        }
+
+        public StockController()
+        {
         }
 
         [HttpGet("value")]
@@ -23,19 +30,13 @@ namespace LondonStockExchange.DataProcessing.Read.Api.Controllers
                 return BadRequest("TickerSymbol cannot be null or empty.");
             }
 
-            var sql = @"SELECT TOP 1 [TickerSymbol], [TradeDateTime], [Price],[Currency] 
-                        FROM[LondonStockExchange_Transactions_Writes].[dbo].[Transactions]
-                        WHERE TickerSymbol = @TickerSymbol
-                        ORDER BY TradeDateTime DESC";
-            
-            await using var connection = new SqlConnection("Server=localhost;Database=LondonStockExchange_Transactions_Writes;Integrated Security=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=True");
-            await connection.OpenAsync();
-
-            var lastStockPrice = await connection.QueryFirstOrDefaultAsync<object>(sql, new { TickerSymbol = tickerSymbol});
-
-            return Ok(lastStockPrice);
+            var latestStockValue = await stockRepository.GetValueByTickerSymbol(tickerSymbol);
+            if(latestStockValue == null)
+            {
+                return NotFound();
+            }
+            return Ok(latestStockValue);
         }
-
 
         [HttpGet("/stocks/value/all")]
         public async Task<ActionResult<IEnumerable<dynamic>>> GetAllStockValues(int pageNumber, int pageSize)
@@ -43,52 +44,29 @@ namespace LondonStockExchange.DataProcessing.Read.Api.Controllers
             if (pageNumber <= 0) { return BadRequest("PageNumber less than 1 is not allowed."); }
             if (pageSize <= 0){ return BadRequest("PageSize less than 1 is not allowed"); }
             if (pageSize > 50){ return BadRequest("PageSize more than 50 is not alloweded."); }
-            
-            var sql = @"Select t1.TickerSymbol, t1.Price, t1.Currency, t1.TradeDateTime
-                        from [LondonStockExchange_Transactions_Writes].[dbo].[Transactions] as t1
-                        inner join 
-                        (
-                            SELECT t2.TickerSymbol, MAX(t2.[TradeDateTime]) as LastestDate
-		                    FROM [LondonStockExchange_Transactions_Writes].[dbo].[Transactions] as t2
-		                    Group by t2.TickerSymbol 
-					        Order By t2.TickerSymbol
-					        OFFSET (@PageNumber-1)*@PageSize ROWS
-					        FETCH NEXT @PageSize ROWS ONLY
-                        ) t 
-                        on t1.TradeDateTime = t.LastestDate
-                        and t1.TickerSymbol = t.TickerSymbol";
 
-            await using var connection = new SqlConnection("Server=localhost;Database=LondonStockExchange_Transactions_Writes;Integrated Security=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=True");
-            await connection.OpenAsync();
-
-            var lastStockPricesForTickers = await connection.QueryAsync<object>(sql, new { PageNumber = pageNumber, PageSize = pageSize });
-
-            return Ok(lastStockPricesForTickers);
+            var latestStockValues = await stockRepository.GetValuesForAllTickers(pageNumber, pageSize);
+            if(!latestStockValues.Any())
+            {
+                return NotFound();
+            }
+            return Ok(latestStockValues);
         }
 
         [HttpGet("/stocks/value")]
-        public async Task<ActionResult<IEnumerable<dynamic>>> GetStockValueByTickerSymbols([FromQuery] IList<string> tickerSymbols)
+        public async Task<ActionResult<IEnumerable<dynamic>>> GetStockValueByTickerSymbols([FromQuery] IEnumerable<string> tickerSymbols)
         {
-            var sql = @"
-                Select t1.TickerSymbol, t1.Price, t1.Currency, t1.TradeDateTime
-                from [LondonStockExchange_Transactions_Writes].[dbo].[Transactions] as t1
-                inner join 
-                (
-                    SELECT t2.TickerSymbol, MAX(t2.[TradeDateTime]) as LastestDate
-		            FROM [LondonStockExchange_Transactions_Writes].[dbo].[Transactions] as t2
-		            WHERE t2.TickerSymbol In @TickerSymbols
-		            Group by t2.TickerSymbol 
-                ) t 
-                on t1.TradeDateTime = t.LastestDate
-                and t1.TickerSymbol = t.TickerSymbol
-                ";
+            if(!tickerSymbols.Any())
+            {
+                return BadRequest("At least one TickerSymbol is required.");
+            }
 
-            await using var connection = new SqlConnection("Server=localhost;Database=LondonStockExchange_Transactions_Writes;Integrated Security=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=True");
-            await connection.OpenAsync();
-
-            var lastStockPricesForTickers = await connection.QueryAsync<object>(sql, new { TickerSymbols = tickerSymbols.ToArray()});
-           
-            return Ok(lastStockPricesForTickers);
+            var latestStockValues = await stockRepository.GetValuesByTickerSymbols(tickerSymbols);
+            if(!latestStockValues.Any())
+            {
+                return NotFound();
+            }
+            return Ok(latestStockValues);
         }
     }
 }
--- LondonStockExchange.DataProcessing.Read.Api/Program.cs
+++ LondonStockExchange.DataProcessing.Read.Api/Program.cs
@@ -1,3 +1,5 @@
+using LondonStockExchange.DataProcessing.Read.Api.Infrastructure.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IStockRepository>(r => new StockRepository("Server=localhost;Database=LondonStockExchange_Transactions_Writes;Integrated Security=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=True"));
 
 var app = builder.Build();
 
