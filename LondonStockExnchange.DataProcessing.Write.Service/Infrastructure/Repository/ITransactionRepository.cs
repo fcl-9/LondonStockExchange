6cc1a5773f8edcf10e37cd3ddb280a0e228d0fbe
fcl-93 <fabio_cl93_@hotmail.com>
2022-02-16 18:45:13 +0000

    TransactionRepository

--- LondonStockExnchange.DataProcessing.Write.Service/Infrastructure/Repository/ITransactionRepository.cs
+++ LondonStockExnchange.DataProcessing.Write.Service/Infrastructure/Repository/ITransactionRepository.cs
@@ -1,9 +1,4 @@
 ﻿using LondonStockExchange.DataProcessing.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LondonStockExnchange.DataProcessing.Write.Service.Infrastructure.Repository
 {
